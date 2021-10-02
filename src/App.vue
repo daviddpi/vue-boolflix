67713a48da55970ad6182eec18ee038302f84cc5
daviddpi <piscopodavid97@gmail.com>
2021-10-02 23:41:36 +0200

    funzionalità ricerca serie tv completata

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <input type="text" v-model="search" @keyup.enter="sendSearch">
     <div>
       <div v-for="(movie, index) in storeMovie" :key="index">
-        {{ movie.title }} , {{movie.original_title}} , {{movie.vote_averange}}
+        {{ movie.title ? movie.title : movie.name }} , {{movie.original_title ? movie.original_title : movie.original_name}} , {{movie.vote_average}}
         <div v-if="confirmImg(movie)">
           <img :src="movie.original_language | convertFlag()" alt="">
         </div>
@@ -18,22 +18,18 @@
 
 <script>
 import axios from 'axios'
-
 export default {
   name: 'App',
   components: {
     
   },
-
   data(){
     return{
       search: "",
       storeMovie: [],
     }
   },
-
   methods: {
-
     getMoviesAndSeries(){
       const addMovie = axios.get("https://api.themoviedb.org/3/search/movie", {
           params: {
@@ -48,11 +44,9 @@ export default {
             query: this.search,
           }
       });
-
       axios.all([addMovie, addSeries]).then(axios.spread( (...responses) => {
         const resultOne = responses[0].data.results;
         const resultTwo = responses[1].data.results;
-
         console.log("La risposta API ", responses);
         console.log("Il primo ",resultOne);
         console.log("Il secondo ",resultTwo);
@@ -61,13 +55,11 @@ export default {
         console.error(errors);
       });
     },
-
     sendSearch(){
       console.log(this.search);
       this.getMoviesAndSeries();
       
     },
-
     /**
      * La funziona passa il valore TRUE in caso la condizione sia verificata, il valore FALSO se non viene verificata
      * La funzione serve per capire se la stringa 'original_language' viene "convertita" in immagine tramite il filters
@@ -83,7 +75,6 @@ export default {
     }
     
   },
-
   
   filters: {
     
@@ -94,20 +85,16 @@ export default {
      * @param str è il valore passato "original language" preso dalla chiatama
      */
     convertFlag(str){
-
       switch (str){
         case "it":
           str = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/800px-Flag_of_Italy.svg.png";
           return str
-
         case "en":
           str = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Flag_of_the_United_Kingdom_%283-5%29.svg/800px-Flag_of_the_United_Kingdom_%283-5%29.svg.png";
           return str
-
         case "de":
           str = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/1280px-Flag_of_Germany.svg.png";
           return str;
-
         case "fr":
           str = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Flag_of_France.svg/1280px-Flag_of_France.svg.png";
           return str;
@@ -121,15 +108,12 @@ export default {
       }
     }
   },
-
 }
 </script>
 
 <style lang="scss">
 @import './style/general.scss';
-
 img{
   width: 25px;
 }
-
 </style>
