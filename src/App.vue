35c1c463d4692429cda435d47cfb3f8ba166a7d9
daviddpi <piscopodavid97@gmail.com>
2021-10-04 16:02:25 +0200

    Creati nuovi componenti, inserito logo e inizializzazione comunicazione tra componenti

--- src/App.vue
+++ src/App.vue
@@ -1,143 +1,38 @@
 <template>
   <div id="app">
-    <input type="text" v-model.trim="search" @keyup.enter="sendSearch">
-    <div>
-      <div v-for="(movie, index) in storeMovieLang" :key="index">
+    <Header @sendSearch="TakenSearch" />
+    <Main :toSearch="needle" />
 
-        <img :src="`https://image.tmdb.org/t/p/w342${movie.backdrop_path}`" alt="">
-        <div>
-          {{ movie.title ? movie.title : movie.name }} , {{movie.original_title ? movie.original_title : movie.original_name}}
-          <div v-if="movie.showFlag != index">
-            <img :src="`https://www.countryflags.io/${movie.original_language}/flat/24.png`" @error="imgNotFound(index)">
-          </div>
-          <div v-else>
-            {{movie.original_language}}
-          </div>
-          <div class="d-flex">
-            <div v-for="(starFull, index) in valutation(movie)" :key="index">
-              <i class="fas fa-star"></i>
-            </div>
-            <div v-for="(starEmpty, index) in (5 - valutation(movie))" :key="index">
-              <i class="far fa-star"></i>
-            </div>
-          </div>
-        </div>
-        <hr>
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+
+
 export default {
   name: 'App',
   components: {
-    
+    Header,
+    Main,
   },
+
   data(){
     return{
-      search: "",
-      storeMovieSerie: [],
+      needle: "",
     }
   },
 
   methods: {
-    getMoviesAndSeries(){
-      const addMovie = axios.get("https://api.themoviedb.org/3/search/movie", {
-          params: {
-            api_key: "401d4009e7dd2687f44c4cf8d0c98098",
-            language: "it-IT",
-            query: this.search,
-
-          }
-        });
-      
-      const addSeries = axios.get("https://api.themoviedb.org/3/search/tv", {
-        params: {
-            api_key: "401d4009e7dd2687f44c4cf8d0c98098",
-            language: "it-IT",
-            query: this.search,
-          }
-      });
-      axios.all([addMovie, addSeries]).then(axios.spread( (...responses) => {
-        const resultOne = responses[0].data.results;
-        const resultTwo = responses[1].data.results;
-        console.log("La risposta API ", responses);
-        console.log("Il primo ",resultOne);
-        console.log("Il secondo ",resultTwo);
-        this.storeMovieSerie = [...resultOne, ...resultTwo];
-        this.storeMovieSerie.forEach( element => {
-          element.showFlag = -1;
-        })
-        console.log(this.storeMovieSerie);
-      })).catch(errors => {
-        console.error(errors);
-      });
+    TakenSearch(needle){
+      console.log(needle);
     },
-
-    sendSearch(){
-      console.log(this.search);
-      this.getMoviesAndSeries();
-    },
-
-    imgNotFound(index){
-      this.storeMovieSerie[index].showFlag = index;
-    },
-
-    valutation(movie){
-      return parseInt( Math.max(movie.vote_average / 2) )
-    }
-    
-  },
-  
-  computed: {
-    /**
-     * La funzione converte la lingua nella bandiera corretta da visualizzare
-     */
-    storeMovieLang(){
-
-      return this.storeMovieSerie.filter( str => {
-        switch(str.original_language){
-          case "en":
-            return str.original_language ="gb";
-  
-          case "ja":
-            return str.original_language = "jp";
-  
-          case "ko":
-            return str.original_language = "kr";
-  
-          case "zh":
-            return str.original_language = "cn";
-          
-          case "hi":
-            return str.original_language = "in";
-  
-          case "ur":
-            return str.original_language = "in";
-  
-          case "uk":
-            return str.original_language = "in";
-          
-          case "cs":
-            return str.original_language = "cz";
-  
-          case "ar":
-            return str.original_language = "ae";
-  
-          default:
-            return str.original_language;
-        }
-      })
-    },
-
   }
 }
 </script>
 
 <style lang="scss">
 @import './style/general.scss';
-
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <div class="d-flex align-items-center justify-content-between">
+        <img src="https://fontmeme.com/temporary/0a43aa36584f2895324a0006b42b5d1a.png" alt="Logo Boolfilx">
+        <input class="search-film" type="text" v-model.trim="search" @keyup.enter="$emit('sendSearch', search )" placeholder="Cerca film o serie Tv">
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    data(){
+        return{
+            search: "",
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+
+header{
+    background-color: black;
+}
+
+.search-film{
+    height: 20%;
+    margin-right: 50px;
+
+}
+
+</style>
